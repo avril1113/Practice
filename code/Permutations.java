068acf3e167c5141acc9edb15d476e1e107cd4c8
Anny Chung <avril1113@gmail.com>
2018-10-22 01:15:00 -0400

    Update the solution

--- code/Permutations.java
+++ code/Permutations.java
@@ -1,22 +1,25 @@
 class Solution {
-    HashSet<List<Integer>> set = new HashSet(); 
+    List<List<Integer>> output = new ArrayList(); 
     public List<List<Integer>> permuteUnique(int[] nums) {
         if (nums == null || nums.length == 0)
-            return new ArrayList<>(set);
+            return output;
         
+        Arrays.sort(nums);
         getPermutations(nums, 0, new ArrayList<Integer>());
         
-        return new ArrayList<>(set);
+        return output;
     }
     
     void getPermutations(int[] array, int index, List temp) {
         for (int i = 0; i <= index; i++) {
             List<Integer> permutation = new ArrayList(temp);
-            permutation.add(i, array[index]);
             
+            if (i > 0 && permutation.get(i-1) == array[index])
+                return;
+            
+            permutation.add(i, array[index]);
             if (permutation.size() == array.length) {
-                if (!set.contains(permutation))
-                    set.add(permutation);
+                output.add(permutation);
             } else {
                 getPermutations(array, index+1, permutation);
             }
