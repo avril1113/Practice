e449fdb874f4fe30fce52c3a81ae70eff9c54bd7
Anny Chung <avril1113@gmail.com>
2019-02-25 23:09:13 -0800

    Add solutions

---
+++ code/ReverseLinkedList_iteration.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null)
+            return head;
+        
+        ListNode pre = null, cur = head, next;
+        while (cur != null) {
+            next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        
+        return pre;
+    }
+}
