2e67725de2fee4e07d58fe543bde4e18993854c3
Anny Chung <avril1113@gmail.com>
2019-07-27 23:43:31 -0700

    Adjust solution

--- code/RotateImage.java
+++ code/RotateImage.java
@@ -1,24 +1,21 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        if (matrix == null || matrix.length == 0 || matrix.length == 1)
+        if (matrix == null || matrix.length < 2)
             return;
         
-        int n = matrix.length;
-        int left = 0, top = 0, right = n-1, bottom = n-1;
+        int start = 0, end = matrix.length-1;
         
-        while (left < right) {
-            for (int i = 0; i < right-left; i++) {
-                int temp = matrix[top][left+i];
-                matrix[top][left+i] = matrix[bottom-i][left];
-                matrix[bottom-i][left] = matrix[bottom][right-i];
-                matrix[bottom][right-i] = matrix[top+i][right];
-                matrix[top+i][right] = temp;
+        while (start < end) {
+            for (int i = 0; i < end-start; i++) {
+                int swap = matrix[start][start+i];
+                matrix[start][start+i] = matrix[end-i][start];
+                matrix[end-i][start] = matrix[end][end-i];
+                matrix[end][end-i] = matrix[start+i][end];
+                matrix[start+i][end] = swap;
             }
             
-            left++;
-            right--;
-            top++;
-            bottom--;
+            start++;
+            end--;
         }
     }
 }
